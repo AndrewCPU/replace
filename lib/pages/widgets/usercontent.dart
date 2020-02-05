02243eb689f6feccfd405e95027cdeeef5dfe7ad
andrew stein <stein.andrew.01@gmail.com>
2020-02-04 22:08:24 -0500

    Text font size change

--- lib/pages/widgets/usercontent.dart
+++ lib/pages/widgets/usercontent.dart
@@ -13,7 +13,7 @@ class _UserGeneratedContentWidgetState extends State<UserGeneratedContentWidget>
     return Card(shape: RoundedRectangleBorder (borderRadius: BorderRadius.circular(30)),elevation: 5,child: SizedBox(width: MediaQuery.of(context).size.width * 0.85, child: Padding(padding: EdgeInsets.all(10), child:
       Center(child:
         Column(children: <Widget>[
-          Icon(Icons.add_circle_outline, size: 150,),
+          OutlineButton(child: Icon(Icons.add, size: 150,)),
         ],),),),),);
   }
 }
